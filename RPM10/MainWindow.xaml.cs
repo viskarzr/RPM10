f09b711762806ed34a7944aa77dea4b0f7b0e6c2
исп-31 <-31@LAB17-05>
2025-11-18 12:09:14 +0300

    логика

--- RPM10/MainWindow.xaml.cs
+++ RPM10/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -19,6 +20,25 @@ namespace RPM10
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private List<Zoo> listAmimals = new List<Zoo>();
+        private Zoo selectedAnimals = null;
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Zoo Animals = null;
+            switch (cbAnimals.Text)
+            {
+                case "Млекопитающие":
+                    Animals = new Mammals();
+                    break;
+                case "Парнокопытные":
+                    Animals = new Artiodactyls();
+                    break;
+                case "Птицы":
+                    Animals = new Birds();
+                    break;
+            }
         }
     }
 }
---
+++ RPM10/Zoo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPM10
+{
+    public abstract class Zoo
+    {
+        public string Name { get; set; }
+        public int Age { get; set; } // сделать ограничение по возрасту и весу
+        public double Weight { get; set; }
+
+        public abstract string Animals { get; }
+        public abstract void UpdateAnimals(string name, int age, double weight);
+
+        public override string ToString()
+        {
+            return $"{Animals}: {Name}, возраст: {Age}, вес: {Weight}.";
+        }
+    }
+
+    public class Mammals : Zoo
+    {
+        public override string Animals
+        {
+            get { return "Mлекопитающее"; }
+        }
+
+        public override void UpdateAnimals(string name, int age, double weight)
+        {
+            Name = name;
+            Age = age;
+            Weight = weight;
+        }
+
+    }
+
+    public class Artiodactyls : Zoo
+    {
+        public override string Animals
+        {
+            get { return "Парнокопытное"; }
+        }
+
+        public override void UpdateAnimals(string name, int age, double weight)
+        {
+            Name = name;
+            Age = age;
+            Weight = weight;
+        }
+    }
+
+    public class Birds:Zoo
+    {
+        public override string Animals
+        {
+            get { return "Птица"; }
+        }
+
+        public override void UpdateAnimals(string name, int age, double weight)
+        {
+            Name = name;
+            Age = age;
+            Weight = weight;
+        }
+
+    }
+
+}
+    
+
